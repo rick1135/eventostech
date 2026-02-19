b639015dfff5e48784a81b875d5eae63758694a5
rick1135 <rick1135@hotmail.com>
2026-02-19 18:05:32 -0300

    feat: criando entidades

---
+++ src/main/java/com/rick1135/eventostech/entity/Address.java
@@ -0,0 +1,28 @@
+package com.rick1135.eventostech.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event_id;
+}
---
+++ src/main/java/com/rick1135/eventostech/entity/Coupon.java
@@ -0,0 +1,28 @@
+package com.rick1135.eventostech.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.*;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date validUntil;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event_id;
+}
