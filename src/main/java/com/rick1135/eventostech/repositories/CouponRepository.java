577d3f8da7831826735b1390f746ea53ba2a4e2f
rick1135 <rick1135@hotmail.com>
2026-02-19 19:51:24 -0300

    fix: melhorando repositories

--- src/main/java/com/rick1135/eventostech/repositories/CouponRepository.java
+++ src/main/java/com/rick1135/eventostech/repositories/CouponRepository.java
@@ -2,8 +2,14 @@ package com.rick1135.eventostech.repositories;
 
 import com.rick1135.eventostech.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface CouponRepository extends JpaRepository<Coupon, UUID> {
+    @Query("SELECT c FROM Coupon c WHERE c.event.id = :eventId AND (c.validUntil IS NULL OR c.validUntil > :currentDate)")
+    List<Coupon> findByEventIdAndValidCoupons(@Param("eventId") UUID eventId, @Param("currentDate") LocalDateTime currentDate);
 }
--- src/main/java/com/rick1135/eventostech/repositories/EventRepository.java
+++ src/main/java/com/rick1135/eventostech/repositories/EventRepository.java
@@ -1,9 +1,14 @@
 package com.rick1135.eventostech.repositories;
 
 import com.rick1135.eventostech.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public interface EventRepository extends JpaRepository<Event, UUID> {
+public interface EventRepository extends JpaRepository<Event, UUID>, JpaSpecificationExecutor<Event> {
+    Page<Event> findByDateAfter(LocalDateTime date, Pageable pageable);
 }
