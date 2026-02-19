06dae6f06ae18d615357e9007d46242086bd6833
rick1135 <rick1135@hotmail.com>
2026-02-19 18:05:18 -0300

    first commit

---
+++ src/test/java/com/rick1135/eventostech/EventostechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick1135.eventostech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventostechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
