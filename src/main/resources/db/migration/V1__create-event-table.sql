6dff42193b453386880b70f1aa4693ab2bb990c4
rick1135 <rick1135@hotmail.com>
2026-02-21 17:34:21 -0300

    fix: sql file

--- src/main/resources/db/migration/V1__create-event-table.sql
+++ src/main/resources/db/migration/V1__create-event-table.sql
@@ -3,9 +3,9 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE event(
     ID UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description VARCHAR(250) NOT NULL,
+    description VARCHAR(250),
     img_url VARCHAR(100) NOT NULL,
-    event_url VARCHAR(100) NOT NULL,
+    event_url VARCHAR(100),
     date TIMESTAMP NOT NULL,
     remote BOOLEAN NOT NULL
 )
