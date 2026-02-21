b7b71d7dcfcaf538fb9a0bcd20b509bf79eb7b20
rick1135 <rick1135@hotmail.com>
2026-02-21 20:18:50 -0300

    fix: main

--- src/main/java/com/rick1135/eventostech/EventostechApplication.java
+++ src/main/java/com/rick1135/eventostech/EventostechApplication.java
@@ -2,8 +2,10 @@ package com.rick1135.eventostech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class EventostechApplication {
 
 	public static void main(String[] args) {
