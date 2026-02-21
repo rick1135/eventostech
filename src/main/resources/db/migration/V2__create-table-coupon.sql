d36c3d22ed9d6ccac1b15cd8c6cec44ce55c45e2
rick1135 <rick1135@hotmail.com>
2026-02-21 17:47:55 -0300

    fix: sql file

--- src/main/resources/db/migration/V2__create-table-coupon.sql
+++ src/main/resources/db/migration/V2__create-table-coupon.sql
@@ -2,7 +2,7 @@ CREATE TABLE coupon(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     code VARCHAR(100) NOT NULL,
     discount INTEGER NOT NULL,
-    valid_until TIMESTAMP NOT NULL,
+    valid_until TIMESTAMP,
     event_id UUID NOT NULL,
     FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
 )
